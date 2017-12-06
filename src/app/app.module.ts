1744e115e9f22dfed699bcdc91b1f857a071cd7d
Zachary Larson <zlarson@thebancorp.com>
2017-12-06 15:33:41 -0600

    POC is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { PrimeNgModule } from './shared/primeng.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
